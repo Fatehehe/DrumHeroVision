9c010f2e77a9dc7a1412bb9144e204b15b15b070
Fatehehe <fatakhillah.khaqo@gmail.com>
2026-06-05 13:02:45 +0700

    Initial Commit

---
+++ DrumHeroVision/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  DrumHeroVision
+//
+//  Created by Fatakhillah Khaqo on 05/06/26.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ DrumHeroVision/DrumHeroVisionApp.swift
@@ -0,0 +1,33 @@
+//
+//  DrumHeroVisionApp.swift
+//  DrumHeroVision
+//
+//  Created by Fatakhillah Khaqo on 05/06/26.
+//
+
+import SwiftUI
+
+@main
+struct DrumHeroVisionApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
